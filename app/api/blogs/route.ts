6786ce4347fa628e79ad0513b54711d3e07a2fe1
hack-lussac <97344769+hack-lussac@users.noreply.github.com>
2026-03-03 13:42:43 +0530

    Fix API routes to always run dynamically

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { blogs } from '@/app/lib/data';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest) {
   const category = req.nextUrl.searchParams.get('category');
   const page = Number(req.nextUrl.searchParams.get('page') ?? 1);
